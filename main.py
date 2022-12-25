ec8526784b7a553d96aea860f2d0d83c128c5872
Yashwanth Sai <68785131+theyashwanthsai@users.noreply.github.com>
2022-12-25 12:30:57 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,35 +1,68 @@
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 
+app = Ursina()
 
+
+
+grass_texture = load_texture('assets/grass_block.png')
+stone_texture = load_texture('assets/stone_block.png')
+brick_texture = load_texture('assets/brick_block.png')
+dirt_texture = load_texture('assets/dirt_block.png')
+sky_texture = load_texture('assets/skybox.png')
+block_pick = 1
+
+def update():
+    global block_pick
+    if held_keys['1']: block_pick = 1
+    if held_keys['2']: block_pick = 2
+    if held_keys['3']: block_pick = 3
+    if held_keys['4']: block_pick = 4
+    
 
 
 class Voxel(Button):
-    def __init__(self, position = (0, 0, 0) ):
+    def __init__(self, position = (0, 0, 0), texture = grass_texture ):
         super().__init__(
             parent = scene,
             position = position,
-            model = 'cube',
+            model = 'assets/block',
             origin_y = 0.5,
-            texture = 'white_cube',
+            texture = texture,
             color = color.color(0, 0, random.uniform(0.9, 1)),
-            highlight_color = color.lime
+            scale = 0.5
         )
 
     def input(self, key):
         if self.hovered:
             if key == 'left mouse down':
-                voxel = Voxel(position = self.position + mouse.normal)
+                if block_pick == 1: voxel = Voxel(position = self.position + mouse.normal, texture = grass_texture)
+                if block_pick == 2: voxel = Voxel(position = self.position + mouse.normal, texture = brick_texture)
+                if block_pick == 3: voxel = Voxel(position = self.position + mouse.normal, texture = stone_texture)
+                if block_pick == 4: voxel = Voxel(position = self.position + mouse.normal, texture = dirt_texture)
             if key == 'right mouse down':
                 destroy(self)
 
-app = Ursina()
+class Sky(Entity):
+    def __init__(self):
+        super().__init__(
+            parent = scene,
+            model = 'sphere',
+            texture = sky_texture,
+            scale = 150,
+            double_sided = True
+            
+            
+        )
 
-for z in range(16):
-    for x in range(16):
+for z in range(20):
+    for x in range(20):
         voxel = Voxel(position = (x, 0, z))
 
 
 
-player = FirstPersonController()        
+player = FirstPersonController()     
+sky = Sky()   
 app.run()
+
+
